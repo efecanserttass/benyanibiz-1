f667511411b74fc5d395e54949168b6c968d24b7
YyPa <40070476+YyPa@users.noreply.github.com>
2019-03-09 15:11:44 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -8,7 +8,7 @@ exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.author.sendCode('asciidoc', ` Komutlar \n\n Yetkili Komutları\n\n b-dmduyuru\n b-temizle (sayı)\n\n Kullanıcı Komutları\n\n b-radyo\n b-sor\n b-çiz\n b-pgif`);
+    message.author.sendCode('asciidoc', ` Komutlar \n\n Kullanıcı Komutları\n\n b-radyo\n b-sor\n b-çiz\n b-pgif`);
   if (message.channel.type !== 'dm') {
     const ozelmesajkontrol = new Discord.RichEmbed()
     .setColor(`RANDOM`)
@@ -30,7 +30,7 @@ exports.run = (client, message, params) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['h', 'halp', 'help', 'y'],
+  aliases: ['h', 'halp', 'help', 'y', 'kyardım', 'kullanıcı'],
   permLevel: 0
 };
 
