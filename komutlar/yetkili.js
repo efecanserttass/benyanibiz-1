3a09507c873016082e110013e52ca7cf0b42032f
YyPa <40070476+YyPa@users.noreply.github.com>
2019-03-09 15:12:53 +0300

    Create yetkili.js

---
+++ komutlar/yetkili.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+exports.run = (client, message, params) => {
+
+  if (!params[0]) {
+    const commandNames = Array.from(client.commands.keys());
+    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+    message.author.sendCode('asciidoc', ` Yetkili Komutlar \n\nb-dmduyuru\n b-temizle (sayı)`);
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(`RANDOM`)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Mesajlarını kontrol et. 💌')
+	.setFooter('Bütün Komutlarımı Yolladım İyiye Kullanman Dileğimizle :)');
+    message.channel.sendEmbed(ozelmesajkontrol) }
+  } else {
+    let command = params[0];
+    if (client.commands.has(command)) {
+      command = client.commands.get(command);
+      message.author.sendCode('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
+    }
+  }
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['h', 'halp', 'help', 'y', 'kyardım', 'kullanıcı'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'yardım',
+  description: 'Tüm komutları gösterir.',
+  usage: 'yardım [komut]'
+};
